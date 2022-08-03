c75cdc6efaaef9b45f0b0859d042b71d98c2c089
Fernando Rafael da Silva <41931386+FernandoRafael26@users.noreply.github.com>
2022-08-03 19:59:13 -0300

    Finalização do Projeto
    projeto Finalizado do Desafio de JAva com Spring Boot

---
+++ Desafio-Dio-Java-Spring-Framework/src/test/java/DesafioDioJavaSpringFramework/DesafioDioJavaSpringFrameworkApplicationTests.java
@@ -0,0 +1,13 @@
+package DesafioDioJavaSpringFramework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioDioJavaSpringFrameworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
